dc72db45e98b3b621559f8a0aaaaa5b80559d09f
cultstatue <saltedchurros@gmail.com>
2022-06-04 06:01:48 -0400

    sorted true/false responses and chosen password length into an onject inside a function

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+// random values
+var randomNumber= function(min, max) {
+
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+
+}
 
 // upercase value array
 var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
@@ -23,88 +31,72 @@ var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
 var lowerCase = upperCase.map(letter => letter.toLowerCase());
 
 // numeric value array
-var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // special character value array
 var specialCharacter = ['?', '!', '%', '@', '&', '=', '+', '-', '#', '~']
 
-  
-console.log(upperCase);
-console.log(lowerCase);
-console.log(number);
-console.log(specialCharacter);
 
-// function to generate random value between defined minimum and maximum
 
-// function to generate password
+// function to prompt password value choices
 var valuePrompt = function() {
+
+  // ask user desired password length
+  var choosePasswordLength = window.prompt("Enter a desired length for your password using a numeric value between 8 to 128.");
+
+    // exclude certain values
+    if (choosePasswordLength < 8 || choosePasswordLength > 128 || isNaN(choosePasswordLength)) {
+
+      window.alert("Please enter a valid number between 8 to 128.");
+
+      // return to start of valuePrompt function
+      return valuePrompt();
+
+    }
 
   // ask user if they want uppercase letters
   var upperCaseConfirm = window.confirm("Would you like to include uppercase letters?");
 
-    // if true include upperCase in possible password values
-    if(upperCaseConfirm) {
-
-      console.log("upper case yes");
-
-    }
-    // else do not include upperCase values
-    else {
-
-      console.log("upper case no");
-
-    }
-
   // ask user if they want lowercase letters
   var lowercaseConfirm = window.confirm("Would you like to include lower case letters?");
-
-     // if TRUE include lowerCase in possible password values
-     if(lowercaseConfirm) {
-
-      console.log(" lower case yes");
-
-     }
-     // else do not include lowerCase values
-     else {
-
-      console.log("lower case no");
-
-     }
 
   // ask user if they want numeric values
   var numberConfirm = window.confirm("Would you like to include numbers?");
 
-     // if TRUE include Numeric values
-     if(numberConfirm) {
-
-      console.log("numbers yes");
-
-     }
-     // else do not include Numeric values
-     else {
-
-      console.log("numbers no");
-     }
-
   // ask user if they want special character values
   var specialConfirm = window.confirm("Would you like to include special characters?");
+
   
-     // if True include specialCharacters in possible password values
-     if(specialConfirm) {
+  var passwordPreferences = {
 
-      console.log("special characters yes");
+    length: choosePasswordLength,
+    upper: upperCaseConfirm,
+    lower: lowercaseConfirm,
+    number: numberConfirm,
+    special: specialConfirm,
+    
+  }
+  
+  console.log(passwordPreferences);
 
-     }
-     // else do not include specialCharacters values
-     else {
-
-      console.log("special characters no");
-
-     }
-
-  // execute function to generate password
-
+  return;
+  
 };
 
-//valuePrompt();
 
+
+
+// function to generate password
+var d = function() {
+
+  for (i = 0; i < passwordLength; i++) {
+
+   var randomUpperCase = upperCase[randomNumber(0,(upperCase.length - 1))];
+
+   console.log(randomUpperCase);
+
+  }
+
+}
+
+valuePrompt();
